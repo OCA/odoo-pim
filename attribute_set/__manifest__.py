509c77edc68e07d194b9f50977a80969d3ca9a00
clementmbr <clement.mombereau@akretion.com.br>
2020-05-18 17:44:11 -0300

    [REF] rename modules based on attribute_set and pim
    New organization :
    
    - attribute_set (former base_custom_attribute)
    - product_attribute_set (former pim_custom_attribute but without menus)
    - pim (The "PIM application" former pim_base)
    - pim_attrubute_set (depends on product_attribute_set and adds menus in
    the PIM application)

---
+++ attribute_set/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "attribute_set",
+    "version": "12.0.0.0.1",
+    "category": "Generic Modules/Others",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "https://akretion.com",
+    "depends": ["base", "base_sparse_field"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/attribute_security.xml",
+        "views/menu_view.xml",
+        "views/attribute_attribute_view.xml",
+        "views/attribute_group_view.xml",
+        "views/attribute_option_view.xml",
+        "views/attribute_set_view.xml",
+        "wizard/attribute_option_wizard_view.xml",
+    ],
+    "external_dependencies": {"python": ["unidecode"]},
+}
