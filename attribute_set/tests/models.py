b9fc0b58acc1a65ee6e23c3226974dd2c03e3aa6
clementmbr <clement.mombereau@akretion.com.br>
2020-05-18 17:44:11 -0300

    [REF] rename modules based on attribute_set and pim
    New organization :
    
    - attribute_set (former base_custom_attribute)
    - product_attribute_set (former pim_custom_attribute but without menus)
    - pim (The "PIM application" former pim_base)
    - pim_attrubute_set (depends on product_attribute_set and adds menus in
    the PIM application)

---
+++ attribute_set/tests/models.py
@@ -0,0 +1,11 @@
+# Copyright 2020 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import models
+
+
+class ResPartner(models.Model):
+    _inherit = ["res.partner", "attribute.set.owner.mixin"]
+    _name = "res.partner"
