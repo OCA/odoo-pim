3d283ce6fa46743b22db62a24e6f41b0d26a9d3f
clementmbr <clement.mombereau@akretion.com.br>
2020-05-27 13:14:34 -0300

    [IMP] remove class in super() and other misc improvements

--- pim/__manifest__.py
+++ pim/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "PIM",
+    "name": "Product Information Management",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion",
