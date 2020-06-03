10639bb500ae26bfabf6e94ec1a6e90755844d50
clementmbr <clement.mombereau@akretion.com.br>
2020-06-03 11:09:09 -0300

    [REF] remove @api.multi

--- product_attribute_set/models/product_category.py
+++ product_attribute_set/models/product_category.py
@@ -2,7 +2,7 @@
 # @author Benoit Guillot <benoit.guillot@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductCategory(models.Model):
@@ -14,7 +14,6 @@ class ProductCategory(models.Model):
         context={"default_model_id": "product.template"},
     )
 
-    @api.multi
     def write(self, vals):
         """Fill Category's products with Category's default attribute_set_id if empty"""
         super().write(vals)
