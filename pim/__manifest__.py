830d1a2516081f7736049f188eec12b4dcf79eaa
Cédric Pigeon <cedric.pigeon@acsone.eu>
2020-06-08 18:06:12 +0200

    [12.0][ADD] add new addon attribute_set_searchable

--- pim/__manifest__.py
+++ pim/__manifest__.py
@@ -7,7 +7,7 @@
     "license": "AGPL-3",
     "author": "Akretion",
     "website": "http://www.akretion.com/",
-    "depends": ["product", "product_attribute_set"],
+    "depends": ["attribute_set_searchable", "product", "product_attribute_set"],
     "data": [
         "data/ir_module_category_data.xml",
         "security/pim_security.xml",
