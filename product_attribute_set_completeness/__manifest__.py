2ba8fc09b3f6c2df964dfd72dc87a505c32a34e7
Cédric Pigeon <cedric.pigeon@acsone.eu>
2020-06-10 16:53:45 +0200

    [ADD] add new features to manage completion rate and completion state on models linked to attribute set

---
+++ product_attribute_set_completeness/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Attribute Set Completeness",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": ["product_attribute_set", "attribute_set_completeness"],
+    "data": ["views/product_template_view.xml"],
+    "demo": [],
+    "auto_install": True,
+}
---
+++ product_attribute_set_completeness/readme/USAGE.rst
@@ -0,0 +1,2 @@
+Completion rate and completion state are visible on the product form.
+These 2 properties are also searchable through the search view ("Complete" and "Not Complete" filters).
