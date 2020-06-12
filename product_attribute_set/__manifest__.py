4c2d80ace1d04bc6caeac2ec72c31aeb51052834
Denis Roussel <denis.roussel@acsone.eu>
2020-06-12 11:23:58 +0200

    [13.0] Set modules version to 13

--- product_attribute_set/__manifest__.py
+++ product_attribute_set/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product Attribute Set",
-    "version": "12.0.2.0.0",
+    "version": "13.0.2.0.0",
     "category": "Generic Modules/Others",
     "license": "AGPL-3",
     "author": "Akretion",
