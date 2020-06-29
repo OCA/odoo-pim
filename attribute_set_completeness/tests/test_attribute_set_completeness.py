2bb6dd6bd0071828329b11d871fc3befa8685086
Denis Roussel <denis.roussel@acsone.eu>
2020-06-29 18:06:30 +0200

    [10.0][IMP] pre-commit updates

--- attribute_set_completeness/tests/test_attribute_set_completeness.py
+++ attribute_set_completeness/tests/test_attribute_set_completeness.py
@@ -37,22 +37,8 @@ class TestAttributeSetCompleteness(SavepointCase):
             "model_id": cls.model_id,
             "attribute_ids": [(4, cls.attr1.id), (4, cls.attr2.id)],
             "attribute_set_completeness_ids": [
-                (
-                    0,
-                    0,
-                    {
-                        "field_id": cls.attr1.field_id.id,
-                        "completion_rate": 50.0,
-                    },
-                ),
-                (
-                    0,
-                    0,
-                    {
-                        "field_id": cls.attr2.field_id.id,
-                        "completion_rate": 50.0,
-                    },
-                ),
+                (0, 0, {"field_id": cls.attr1.field_id.id, "completion_rate": 50.0}),
+                (0, 0, {"field_id": cls.attr2.field_id.id, "completion_rate": 50.0}),
             ],
         }
         cls.attr_set = cls.env["attribute.set"].create(vals)
@@ -73,22 +59,8 @@ class TestAttributeSetCompleteness(SavepointCase):
             "model_id": self.model_id,
             "attribute_ids": [(4, self.attr1.id), (4, self.attr2.id)],
             "attribute_set_completeness_ids": [
-                (
-                    0,
-                    0,
-                    {
-                        "field_id": self.attr1.field_id.id,
-                        "completion_rate": 50.0,
-                    },
-                ),
-                (
-                    0,
-                    0,
-                    {
-                        "field_id": self.attr2.field_id.id,
-                        "completion_rate": 10.0,
-                    },
-                ),
+                (0, 0, {"field_id": self.attr1.field_id.id, "completion_rate": 50.0}),
+                (0, 0, {"field_id": self.attr2.field_id.id, "completion_rate": 10.0}),
             ],
         }
         with self.assertRaises(ValidationError):
@@ -99,14 +71,7 @@ class TestAttributeSetCompleteness(SavepointCase):
         vals = {
             "attribute_set_completeness_ids": [
                 (2, completion_rules[0].id),
-                (
-                    0,
-                    0,
-                    {
-                        "field_id": self.attr1.field_id.id,
-                        "completion_rate": 10.0,
-                    },
-                ),
+                (0, 0, {"field_id": self.attr1.field_id.id, "completion_rate": 10.0}),
             ]
         }
 
@@ -118,14 +83,7 @@ class TestAttributeSetCompleteness(SavepointCase):
         vals = {
             "attribute_set_completeness_ids": [
                 (2, completion_rules[0].id),
-                (
-                    0,
-                    0,
-                    {
-                        "field_id": self.attr1.field_id.id,
-                        "completion_rate": 200.0,
-                    },
-                ),
+                (0, 0, {"field_id": self.attr1.field_id.id, "completion_rate": 200.0},),
             ]
         }
 
