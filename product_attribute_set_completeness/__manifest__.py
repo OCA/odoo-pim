0c05657fc7773387b2a98128cbdc72284b342dbc
Simone Orsi <simahawk@gmail.com>
2020-10-08 10:11:53 +0200

    [MIG] product_attribute_set_completeness: Migration to 13.0

--- product_attribute_set_completeness/__manifest__.py
+++ product_attribute_set_completeness/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Attribute Set Completeness",
-    "version": "12.0.1.2.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu",
