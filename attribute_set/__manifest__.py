cb5e2f8ca8eac4ce337e9adf079e7b896cb1b259
Quentin Groulard <quentin.groulard@acsone.eu>
2020-11-12 12:36:21 +0100

    [IMP] Add attribute widget field
    [UPD] Update attribute_set.pot
    
    attribute_set 14.0.1.1.0

--- attribute_set/__manifest__.py
+++ attribute_set/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Attribute Set",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.1.0",
     "category": "Generic Modules/Others",
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
