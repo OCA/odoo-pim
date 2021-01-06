a5266f7d4f22cb226cce41b1673a6acc348609b8
Pierrick Brun <pierrick.brun@akretion.com>
2021-01-06 11:27:27 +0100

    [IMP] product_attribute_set: black, isort, prettier

--- product_attribute_set/__manifest__.py
+++ product_attribute_set/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product Attribute Set",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules/Others",
     "license": "AGPL-3",
     "author": "Akretion",
@@ -10,5 +10,5 @@
     "depends": ["product", "attribute_set"],
     "data": ["views/product.xml", "views/product_category.xml"],
     "demo": ["demo/product_attribute.xml"],
-    "installable": False,
+    "installable": True,
 }
