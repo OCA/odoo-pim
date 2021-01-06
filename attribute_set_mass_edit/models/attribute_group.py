0de2393842f7a73860cef0ffd5187bc77279cdd9
Pierrick Brun <pierrick.brun@akretion.com>
2021-01-06 16:00:50 +0100

    [IMP] attribute_set_mass_edit: black, isort, prettier

--- attribute_set_mass_edit/models/attribute_group.py
+++ attribute_set_mass_edit/models/attribute_group.py
@@ -1,28 +1,30 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
-from odoo.fields import first
+from odoo import fields, models
 
 
 class AttributeGroup(models.Model):
-
     _inherit = "attribute.group"
+
+    mass_edit_action_ids = fields.One2many(
+        "ir.actions.server", inverse_name="mass_edit_attribute_group_id"
+    )
 
     def write(self, vals):
         res = super(AttributeGroup, self).write(vals)
         if "name" in vals.keys():
-            mass_editings = self.env["mass.editing"].search(
-                [("attribute_group_id", "in", self.ids)]
+            mass_editings = self.env["ir.actions.server"].search(
+                [("mass_edit_attribute_group_id", "in", self.ids)]
             )
             for group in self:
-                mass_editing = first(
-                    mass_editings.filtered(lambda o: o.attribute_group_id == group)
+                mass_editing = fields.first(
+                    mass_editings.filtered(
+                        lambda o: o.mass_edit_attribute_group_id == group
+                    )
                 )
                 if mass_editing:
                     mass_editing.name = group.name
-                    # TODO: we should use tests.Form IMO
-                    mass_editing.action_name = mass_editing._prepare_action_name()
-                    mass_editing.disable_mass_operation()
-                    mass_editing.enable_mass_operation()
+                    # mass_editing.disable_mass_operation()
+                    # mass_editing.enable_mass_operation()
         return res
