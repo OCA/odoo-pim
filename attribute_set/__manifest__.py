df92088252d9f46dcfc06f6f05723dd2bf03129a
Pierrick Brun <pierrick.brun@akretion.com>
2021-02-22 11:50:20 +0100

    [IMP] attribute_set: black, isort, prettier

--- attribute_set/__manifest__.py
+++ attribute_set/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Attribute Set",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules/Others",
     "license": "AGPL-3",
     "author": "Akretion",
@@ -17,5 +17,5 @@
         "wizard/attribute_option_wizard_view.xml",
     ],
     "external_dependencies": {"python": ["unidecode"]},
-    "installable": False,
+    "installable": True,
 }
