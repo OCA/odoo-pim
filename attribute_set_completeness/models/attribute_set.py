3e5b27d989a3e934711d8327bddbf1c43ae07bd5
Kévin Roche <kevin.roche@akretion.com>
2021-03-14 21:03:57 +0100

    Add automatic completion rates

--- attribute_set_completeness/models/attribute_set.py
+++ attribute_set_completeness/models/attribute_set.py
@@ -16,11 +16,44 @@ class AttributeSet(models.Model):
         auto_join=True,
     )
 
+    # Add a button to let the user choose between automatic completion rate vs manual input
+    completeness_mode = fields.Boolean(default=True)
+
+    # If automatic mode, each attribut will have the same completion rate.
+    # Taking account of the possible remainder depending of 100 divised
+    # by the number of attributes.
+    @api.onchange("completeness_mode")
+    def _auto_rate_completeness(self):
+        for rec in self:
+            if rec.completeness_mode:
+                for attr_set in self:
+                    completion_config = attr_set.attribute_set_completeness_ids
+                    if completion_config:
+                        list_attr = [
+                            attr.completion_rate for attr in completion_config
+                        ]
+                        attributs_num = len(completion_config)
+                        value_attr = 100 / attributs_num
+                        for attr in completion_config:
+                            attr.completion_rate = int(value_attr)
+                        total = sum(list_attr)
+                        if total != 100:
+                            remainder = 100 % attributs_num
+                            first_attr = completion_config[0]
+                            first_attr.completion_rate += remainder
+
     @api.constrains("attribute_set_completeness_ids")
     def _check_attribute_set_completeness_ids(self):
         for attr_set in self:
             completion_config = attr_set.attribute_set_completeness_ids
             if completion_config:
-                total = sum([rule.completion_rate for rule in completion_config])
+                total = sum(
+                    [rule.completion_rate for rule in completion_config]
+                )
                 if total != 100.0:
-                    raise ValidationError(_("Total of completion rate must be 100 %"))
+                    if self.completeness_mode:
+                        self._auto_rate_completeness()
+                    else:
+                        raise ValidationError(
+                            _("Total of completion rate must be 100 %")
+                        )
