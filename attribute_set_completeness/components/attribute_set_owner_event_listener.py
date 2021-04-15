7b898b3f4ce33aff32af7088e062196ab6f00971
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-04-15 10:46:56 +0200

    [14.0][MIG] attribute_set_completeness
    * Split computed methods in attribute.set.owner.mixin.
    * Use assertRaisesRegex in tests to check for exact error message.
    * Improve readability of event listner skip_if conditions.

--- attribute_set_completeness/components/attribute_set_owner_event_listener.py
+++ attribute_set_completeness/components/attribute_set_owner_event_listener.py
@@ -1,5 +1,6 @@
 # Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo.addons.component.core import AbstractComponent
 from odoo.addons.component_event import skip_if
 
@@ -8,26 +9,23 @@ class AttributeSetOwnerEventListener(AbstractComponent):
     _name = "attribute.set.owner.event.listener"
     _inherit = "base.event.listener"
 
+    def _get_skip_if_condition_fields(self, record):
+        """Return the field names that trigger the condition"""
+        attribute_set = record.attribute_set_id
+        attribute_set_completeness = attribute_set.attribute_set_completeness_ids
+        field_names = attribute_set_completeness.mapped("field_id.name")
+        field_names.append("attribute_set_id")
+        return field_names
+
     def _get_skip_if_condition(self, record, **kwargs):
         if not record.attribute_set_id:
             return True
-        fields = kwargs["fields"]
-        if "attribute_set_id" in fields or any(
-            [
-                field
-                for field in fields
-                if field
-                in record.attribute_set_id.attribute_set_completeness_ids.mapped(
-                    "field_id.name"
-                )
-            ]
-        ):
+        if set(self._get_skip_if_condition_fields(record)) & set(kwargs["fields"]):
             return False
-
         return True
 
     @skip_if(
         lambda self, record, **kwargs: self._get_skip_if_condition(record, **kwargs)
     )
     def on_record_write(self, record, fields=None):
-        record._compute_completion_rate()
+        record._compute_attribute_set_completed_ids()
