ee1a7d8d0cbecfe3e257dac14b256fca7a0f0425
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-04-16 10:16:33 +0200

    [14.0][MIG] attribute_set_completeness

--- attribute_set_completeness/models/attribute_set_owner_mixin.py
+++ attribute_set_completeness/models/attribute_set_owner_mixin.py
@@ -14,12 +14,7 @@ class AttributeSetOwnerMixin(models.AbstractModel):
         default="not_complete",
         readonly=True,
     )
-    attribute_set_completeneness_ids = fields.One2many(
-        related="attribute_set_id.attribute_set_completeness_ids", readonly=True,
-    )
-    attribute_set_not_completed_ids = fields.Many2many(
-        comodel_name="attribute.set.completeness", readonly=True
-    )
+
     attribute_set_completeneness_ids = fields.One2many(
         related="attribute_set_id.attribute_set_completeness_ids", readonly=True,
     )
