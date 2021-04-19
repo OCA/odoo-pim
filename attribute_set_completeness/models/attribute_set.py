8dc7aeacbf7cb7246e183f95de4428e2ad547b53
Kévin Roche <kevin.roche@akretion.com>
2021-04-19 16:07:55 +0200

    IMP add tests and code improvements

--- attribute_set_completeness/models/attribute_set.py
+++ attribute_set_completeness/models/attribute_set.py
@@ -4,6 +4,7 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.fields import first
+from odoo.tools import float_compare
 
 
 class AttributeSet(models.Model):
@@ -23,25 +24,24 @@ class AttributeSet(models.Model):
         default=True, help="Equalize all the completion rates to the same percentage",
     )
 
-    # If automatic mode, each attribut will have the same completion rate.
-    # Taking account of the possible remainder depending of 100 divised
+    # If automatic mode, each attribute will have the same completion rate.
+    # Taking account of the possible remainder depending of 100 divided
     # by the number of attributes.
     @api.onchange("is_automatic_rate")
     def _automatic_rate(self):
-        for rec in self.filtered(lambda r: r.is_automatic_rate):
-            if rec.is_automatic_rate:
-                for attr_set in self:
-                    completion_config = attr_set.attribute_set_completeness_ids
-                    if completion_config:
-                        list_attr = [attr.completion_rate for attr in completion_config]
-                        attributs_num = len(completion_config)
-                        value_attr = 100 / attributs_num
-                        for attr in completion_config:
-                            attr.completion_rate = int(value_attr)
-                        total = sum(list_attr)
-                        if total != 100:
-                            remainder = 100 % attributs_num
-                            first(completion_config).completion_rate += remainder
+        for attr_set in self.filtered(lambda r: r.is_automatic_rate):
+            completion_config = attr_set.attribute_set_completeness_ids
+            if completion_config:
+                attributes_num = len(completion_config)
+                value_attr = 100.0 / attributes_num
+                list_attr = []
+                for attr in completion_config:
+                    list_attr.append(attr.completion_rate)
+                    attr.completion_rate = int(value_attr)
+                total = sum(list_attr)
+                if float_compare(total, 100.0, 1):
+                    remainder = 100.0 % attributes_num
+                    first(completion_config).completion_rate += remainder
 
     @api.constrains("attribute_set_completeness_ids")
     def _check_attribute_set_completeness_ids(self):
@@ -49,7 +49,7 @@ class AttributeSet(models.Model):
             completion_config = attr_set.attribute_set_completeness_ids
             if completion_config:
                 total = sum([rule.completion_rate for rule in completion_config])
-                if total != 100.0:
+                if float_compare(total, 100.0, 1):
                     if self.is_automatic_rate:
                         self._automatic_rate()
                     else:
--- attribute_set_completeness/tests/test_attribute_set_completeness.py
+++ attribute_set_completeness/tests/test_attribute_set_completeness.py
@@ -58,6 +58,7 @@ class TestAttributeSetCompleteness(SavepointComponentCase):
     def test_completion_rate_constrains_create(self):
         vals = {
             "name": "My attribute Set Test",
+            "is_automatic_rate": False,
             "model_id": self.model_id,
             "attribute_ids": [(4, self.attr1.id), (4, self.attr2.id)],
             "attribute_set_completeness_ids": [
@@ -71,10 +72,11 @@ class TestAttributeSetCompleteness(SavepointComponentCase):
     def test_completion_rate_constrains_write_low(self):
         completion_rules = self.attr_set.attribute_set_completeness_ids
         vals = {
+            "is_automatic_rate": False,
             "attribute_set_completeness_ids": [
                 (2, completion_rules[0].id),
                 (0, 0, {"field_id": self.attr1.field_id.id, "completion_rate": 10.0}),
-            ]
+            ],
         }
 
         with self.assertRaises(ValidationError):
@@ -83,10 +85,11 @@ class TestAttributeSetCompleteness(SavepointComponentCase):
     def test_completion_rate_constrains_write_high(self):
         completion_rules = self.attr_set.attribute_set_completeness_ids
         vals = {
+            "is_automatic_rate": False,
             "attribute_set_completeness_ids": [
                 (2, completion_rules[0].id),
                 (0, 0, {"field_id": self.attr1.field_id.id, "completion_rate": 200.0},),
-            ]
+            ],
         }
 
         with self.assertRaises(ValidationError):
@@ -114,3 +117,130 @@ class TestAttributeSetCompleteness(SavepointComponentCase):
         partner.invalidate_cache()
         self.assertEqual(partner.completion_state, "complete")
         self.assertEqual(partner.completion_rate, 100.0)
+
+    def test_auto_completion_rate(self):
+        #  test with "is_automatic_rate" == True by default
+        vals = {
+            "nature": "custom",
+            "model_id": self.model_id,
+            "attribute_type": "char",
+            "field_description": "Attribute test3",
+            "name": "x_test3",
+            "attribute_group_id": self.group.id,
+        }
+        self.attr3 = self.env["attribute.attribute"].create(vals)
+
+        vals.update({"name": "x_test4", "field_description": "Attribute test4"})
+        self.attr4 = self.env["attribute.attribute"].create(vals)
+
+        vals.update({"name": "x_test5", "field_description": "Attribute test5"})
+        self.attr5 = self.env["attribute.attribute"].create(vals)
+
+        vals.update({"name": "x_test6", "field_description": "Attribute test6"})
+        self.attr6 = self.env["attribute.attribute"].create(vals)
+
+        vals = {
+            "name": "My attribute Set 2",
+            "model_id": self.model_id,
+            "attribute_ids": [
+                (4, self.attr3.id),
+                (4, self.attr4.id),
+                (4, self.attr5.id),
+                (4, self.attr6.id),
+            ],
+            "attribute_set_completeness_ids": [
+                (0, 0, {"field_id": self.attr3.field_id.id, "completion_rate": 25.0}),
+                (0, 0, {"field_id": self.attr4.field_id.id, "completion_rate": 60.0}),
+                (0, 0, {"field_id": self.attr5.field_id.id, "completion_rate": 8.0}),
+                (0, 0, {"field_id": self.attr6.field_id.id, "completion_rate": 156.0}),
+            ],
+        }
+        self.attr_set2 = self.env["attribute.set"].create(vals)
+
+        partner2 = self.env["res.partner"].create({"name": "Test Partner 2"})
+
+        partner2.write({"attribute_set_id": self.attr_set2.id})
+        partner2.write({"x_test3": "test"})
+        partner2.write({"x_test4": "test"})
+        partner2.write({"x_test5": "test"})
+        partner2.write({"x_test6": "test"})
+
+        # Completion rate = 100 and complete
+        self.assertEqual(partner2.completion_state, "complete")
+        self.assertEqual(partner2.completion_rate, 100.0)
+
+        # Same completion rate for each attribute (100/4 = 25)
+        self.assertEqual(len(partner2.attribute_set_completeneness_ids), 4)
+        self.assertEqual(
+            partner2.attribute_set_completeneness_ids[0].completion_rate, 25.0
+        )
+        self.assertEqual(
+            partner2.attribute_set_completeneness_ids[1].completion_rate, 25.0
+        )
+        self.assertEqual(
+            partner2.attribute_set_completeneness_ids[2].completion_rate, 25.0
+        )
+        self.assertEqual(
+            partner2.attribute_set_completeneness_ids[3].completion_rate, 25.0
+        )
+
+    def test_auto_completion_rate_with_3_attributes(self):
+        # With "is_automatic_rate" == True by default
+        # Sometimes, the total completion_rate divides by the attributes number
+        # can not let equal completion_rates for each attribute and gives a remainder.
+        # For example : 100 / 3 = 33.33333333333...
+        # This remainder will be added to the first attribute when saving the set.
+
+        vals = {
+            "nature": "custom",
+            "model_id": self.model_id,
+            "attribute_type": "char",
+            "field_description": "Attribute test3",
+            "name": "x_test3",
+            "attribute_group_id": self.group.id,
+        }
+        self.attr3 = self.env["attribute.attribute"].create(vals)
+
+        vals.update({"name": "x_test4", "field_description": "Attribute test4"})
+        self.attr4 = self.env["attribute.attribute"].create(vals)
+
+        vals.update({"name": "x_test5", "field_description": "Attribute test5"})
+        self.attr5 = self.env["attribute.attribute"].create(vals)
+
+        vals = {
+            "name": "My attribute Set 3",
+            # "is_automatic_rate": True,
+            "model_id": self.model_id,
+            "attribute_ids": [
+                (4, self.attr3.id),
+                (4, self.attr4.id),
+                (4, self.attr5.id),
+            ],
+            "attribute_set_completeness_ids": [
+                (0, 0, {"field_id": self.attr3.field_id.id, "completion_rate": 20.0}),
+                (0, 0, {"field_id": self.attr4.field_id.id, "completion_rate": 60.0}),
+                (0, 0, {"field_id": self.attr5.field_id.id, "completion_rate": 30.0}),
+            ],
+        }
+        self.attr_set3 = self.env["attribute.set"].create(vals)
+
+        partner3 = self.env["res.partner"].create({"name": "Test Partner 3"})
+
+        partner3.write({"attribute_set_id": self.attr_set3.id})
+        partner3.write({"x_test3": "test"})
+        partner3.write({"x_test4": "test"})
+        partner3.write({"x_test5": "test"})
+
+        # Completion rate = 100 and complete
+        self.assertEqual(partner3.completion_state, "complete")
+        self.assertEqual(partner3.completion_rate, 100.0)
+
+        # 3 attributes give 33 % of completion rate with 1 as remainder.
+        # The remainder will be added to the first attribute when saving the set.
+        self.assertEqual(
+            partner3.attribute_set_completeneness_ids[0].completion_rate, 34.0
+        )
+        self.assertEqual(
+            partner3.attribute_set_completeneness_ids[1].completion_rate,
+            partner3.attribute_set_completeneness_ids[2].completion_rate,
+        )
