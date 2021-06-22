d5cab7eeefbec90f5cde803aa3d5b3f382a463d4
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-06-22 15:38:48 +0200

    [MIG][14.0] odoo-pim/product_search_multi_value
    operator ilike + not ilike removed -> should't be there since the beginning
    operator <> removed -> odoo bug with not in for translated fields so removed
    
    [UPD] Update product_search_multi_value.pot
    
    [UPD] README.rst
    
    [ADD] icon.png

--- product_search_multi_value/tests/test_product_search_multi_value.py
+++ product_search_multi_value/tests/test_product_search_multi_value.py
@@ -1,16 +1,15 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.osv.expression import TRUE_DOMAIN
+from odoo.exceptions import UserError
 from odoo.tests.common import TransactionCase
 
 
 class TestProductSearchMultiValue(TransactionCase):
     def setUp(self):
         super(TestProductSearchMultiValue, self).setUp()
-        self.default_code_list = ["E-COM08", "E-COM10", "CONS_DEL03"]
+        self.default_code_list = ["E-COM08", "E-COM10", "E-COM06"]
 
     def test_search_multi_value(self):
-        total_products = self.env["product.template"].search_count(TRUE_DOMAIN)
         default_code_values = " ".join(self.default_code_list)
 
         domain = [("search_multi", "ilike", default_code_values)]
@@ -22,9 +21,9 @@ class TestProductSearchMultiValue(TransactionCase):
         self.assertEqual(res, 3)
 
         domain = [("search_multi", "not ilike", default_code_values)]
-        res = self.env["product.template"].search_count(domain)
-        self.assertEqual(res, total_products - 3)
+        with self.assertRaises(UserError):
+            self.env["product.template"].search_count(domain)
 
         domain = [("search_multi", "!=", default_code_values)]
-        res = self.env["product.template"].search_count(domain)
-        self.assertEqual(res, total_products - 3)
+        with self.assertRaises(UserError):
+            self.env["product.template"].search_count(domain)
