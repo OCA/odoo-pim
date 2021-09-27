4c0b88bab9818073625c613d5ecb31adf62b0cb6
Cédric Pigeon <cedric.pigeon@acsone.eu>
2021-09-27 20:46:48 +0200

    [14.0][ADD]attribut_set: manage company_dependent attributes

---
+++ attribute_set/models/ir_model.py
@@ -0,0 +1,25 @@
+# Copyright 2021 ACSONE SA/NV (http://www.acsone.eu).
+# @author Cédric PIGEON <cedric.pigeon@acsone.eu>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, models
+
+
+class IrModelFields(models.Model):
+    _inherit = "ir.model.fields"
+
+    @api.model
+    def _instanciate_attrs(self, field_data):
+        attrs = super()._instanciate_attrs(field_data)
+        name = field_data.get("name")
+        if name.startswith("x_"):
+            field_id = field_data.get("id")
+
+            self.env.cr.execute(
+                "SELECT company_dependent FROM attribute_attribute"
+                " WHERE field_id=%s",
+                (field_id,),
+            )
+            result = self.env.cr.fetchone()
+            if result and result[0]:
+                attrs["company_dependent"] = True
+        return attrs
