e4eddf91e6742f51ca584f5271544bed2012a079
Sermeus Steven <sermeus.steven.isj@gmail.com>
2023-05-16 15:49:22 +0200

    Add field search panel module.

---
+++ field_search_panel/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Field Search panel",
+    "category": "Generic Modules/Others",
+    "website": "https://github.com/OCA/odoo-pim",
+    "version": "14.0.1.0.0",
+    "data": [
+        "views/res_config_settings.xml",
+    ],
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+}
---
+++ field_search_panel/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module allows the user to define the field that will appear in the search panel of a model at run time.
+
+It also allows the user to use boolean field in the search panel.
