e31f22490bbcdeeecef49d2e31f4197c674e8e21
onurugur <onurugur@github.com>
2024-06-14 13:26:55 +0300

    [17.0] Migrate

--- attribute_set_completeness/components/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import attribute_set_owner_event_listener
--- pim/readme/CONFIGURE.rst
+++
@@ -1,1 +0,0 @@
-Define your user **PIM access rights** in the Application Accesses as a Manager, User or Reader and the application menu "PIM" will appear.
