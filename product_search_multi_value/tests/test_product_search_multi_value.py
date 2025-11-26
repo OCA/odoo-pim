1b9f0479fa37c2f13380cfef83d9ed6a49e89c13
Mathieu <mathieu.delva@hotmail.com>
2025-11-26 10:01:34 +0100

    [MIG] product_search_multi_value: Migration to 18.0

--- product_search_multi_value/tests/test_product_search_multi_value.py
+++ product_search_multi_value/tests/test_product_search_multi_value.py
@@ -29,7 +29,7 @@ class TestProductSearchMultiValue(TransactionCase):
             self.env["product.template"].search_count(domain)
 
     def test_variant_search_multi_value(self):
-        default_code_values = " FURN_0096 FURN_0097 FURN_0098"
+        default_code_values = "FURN_0096 FURN_0097 FURN_0098"
         domain = [("search_multi", "ilike", default_code_values)]
         res = self.env["product.product"].search_count(domain)
         self.assertEqual(res, 3)
