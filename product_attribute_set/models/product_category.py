747c824a3d8c05acd103892e1ce5dcebdaf41566
bosd <11499387+bosd@users.noreply.github.com>
2026-02-16 19:43:49 +0100

    [IMP] product_attribute_set
    Co-authored-by: Miquel Raïch <miquel.raich@forgeflow.com>

--- product_attribute_set/models/product_category.py
+++ product_attribute_set/models/product_category.py
@@ -16,11 +16,11 @@ class ProductCategory(models.Model):
 
     def write(self, vals):
         # Fill Category's products with Category's default attribute_set_id if empty
-        super().write(vals)
+        res = super().write(vals)
         for record in self:
             if vals.get("attribute_set_id"):
                 templates = self.env["product.template"].search(
                     [("categ_id", "=", record.id), ("attribute_set_id", "=", False)]
                 )
                 templates.write({"attribute_set_id": record.attribute_set_id.id})
-        return True
+        return res
