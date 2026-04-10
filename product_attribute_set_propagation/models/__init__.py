07d045673fc99497cc0133868cea7e5bbedd3a83
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2026-04-10 11:12:54 +0200

    [ADD] product_attribute_set_propagation

---
+++ product_attribute_set_propagation/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2026 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import attribute_attribute
+from . import product_template
+from . import product_product
